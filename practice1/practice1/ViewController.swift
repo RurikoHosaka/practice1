76992ed945e236050e01f8dff5e6ad7b3c28229c
Ruriko Hosaka <hosaka@museit.co.jp>
2020-04-17 14:18:00 +0900

    create: リボジトリ

---
+++ practice1/practice1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  practice1
+//
+//  Created by ruriko hosaka on 2020/04/17.
+//  Copyright © 2020 ruriko hosaka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
